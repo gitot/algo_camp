bee6119a4b6d83a0b71cca40cf55570e756d9fb6
wgy <guiyong.wang@freemud.cn>
2021-12-04 09:56:25 +0800

    S125 AC

--- src/java/com/github/algo/base/solution/base/S125.java
+++ src/java/com/github/algo/base/solution/base/S125.java
@@ -1,41 +1,47 @@
 package com.github.algo.base.solution.base;
 
+/**
+ * 0. please understand the question firstly(improve English)
+ * 1. no need to know the int value that represents the char
+ * 2. no need to use nested loop
+ */
 public class S125 {
 
-    //A-Z 65-90
-    //a-z 97-122
     public boolean isPalindrome(String s) {
         char[] chars = s.toCharArray();
         int left = 0;
-        int right = s.length() - 1;
+        int right = chars.length - 1;
 
-
-        //compare two char on the specified position
-        while (left < right && left >= 0 && left < s.length() && right >= 0 && right < s.length()) {
-            //jump over the chars that is not a alphabet
-            while (!isAlphBet(chars[left])) {
+        while (left < right) {
+            if (!isAlphBetOrNumber(chars[left])) {
                 left++;
-                break;
+                continue;
             }
-            //jump over the chars that is not a alphabet
-            while (!isAlphBet(chars[right])) {
+            if (!isAlphBetOrNumber(chars[right])) {
                 right--;
-                break;
+                continue;
             }
-
-            int abs = Math.abs(chars[left] - chars[right]);
-            if (abs != 0 && abs != 32) {
+            if (toLower(chars[left]) != toLower(chars[right])) {
                 return false;
             }
-
             left++;
             right--;
         }
+
         return true;
     }
 
-    private boolean isAlphBet(char aChar) {
-        return (aChar >= 65 && aChar <= 90) || (aChar >= 97 && aChar <= 122);
+    private char toLower(char c) {
+        if (c >= 'a' && c <= 'z') return c;
+        if (c >= '0' && c <= '9') return c;
+        return (char) (c + 32);
+    }
+
+    private boolean isAlphBetOrNumber(char c) {
+        if (c >= 'a' && c <= 'z') return true;
+        if (c >= 'A' && c <= 'Z') return true;
+        if (c >= '0' && c <= '9') return true;
+        return false;
     }
 
     public static void main(String[] args) {
