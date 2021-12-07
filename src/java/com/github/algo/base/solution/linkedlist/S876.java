b4a38330c0ac064c08534902a8576489834bee98
guyot <1571337712@qq.com>
2021-12-07 20:46:32 +0800

    S876

---
+++ src/java/com/github/algo/base/solution/linkedlist/S876.java
@@ -0,0 +1,13 @@
+package com.github.algo.base.solution.linkedlist;
+
+public class S876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
