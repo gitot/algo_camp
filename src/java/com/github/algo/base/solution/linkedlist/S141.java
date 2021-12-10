1b0e42c70d209060bd29c46bd61e1f616647cdd8
guyot <1571337712@qq.com>
2021-12-10 22:23:39 +0800

    S141

---
+++ src/java/com/github/algo/base/solution/linkedlist/S141.java
@@ -0,0 +1,17 @@
+package com.github.algo.base.solution.linkedlist;
+
+public class S141 {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/java/com/github/algo/base/solution/linkedlist/S160.java
@@ -0,0 +1,11 @@
+package com.github.algo.base.solution.linkedlist;
+
+public class S160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA, b = headB;
+
+        //todo
+
+        return null;
+    }
+}
