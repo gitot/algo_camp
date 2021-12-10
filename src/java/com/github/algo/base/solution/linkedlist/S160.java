c78c47aa89ba727454ab61293fb1b127f9a43901
guyot <1571337712@qq.com>
2021-12-11 01:12:43 +0800

    S160.java

--- src/java/com/github/algo/base/solution/linkedlist/S160.java
+++ src/java/com/github/algo/base/solution/linkedlist/S160.java
@@ -2,10 +2,19 @@ package com.github.algo.base.solution.linkedlist;
 
 public class S160 {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        //The two pointer will finally be equal,
+        //either at the actual node at which the two linked list come across,
+        //or at the end of each linked list.
         ListNode a = headA, b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
 
-        //todo
-
-        return null;
+        return a;
     }
 }
