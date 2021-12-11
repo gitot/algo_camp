c23c6e6bf132736ab80dc101b3735edc6fcd4ca2
wgy <guiyong.wang@freemud.cn>
2021-12-11 12:32:04 +0800

    S25.java

--- src/java/com/github/algo/base/solution/linkedlist/S25.java
+++ src/java/com/github/algo/base/solution/linkedlist/S25.java
@@ -1,36 +1,56 @@
 package com.github.algo.base.solution.linkedlist;
 
+import com.github.algo.base.solution.linkedlist.test.Utils;
+
 public class S25 {
 
     public ListNode reverseKGroup(ListNode head, int k) {
-        //1.find continual k elements
-        //2.reverse these k  elements
-        //3.link the reversed elements to the left
-        //Loop from step 1 to step 3, util there is not enough elements remain on the right
         ListNode sentinel = new ListNode(0, null);
-        ListNode tail = head;
-        sentinel.next = tail;
+        sentinel.next = head;
+        ListNode tail = sentinel;
 
-        while (tail != null) {
+        while (tail != null && tail.next != null) {
             //find k elements
-            int i = k - 1;
-            for (int j = 0; j < i; j++) {
-                if (tail == null)
+            int j = 0;
+            ListNode t = tail;   //the tail at which the reversed list will be linked
+            ListNode h = tail.next;  //the first node of the list that will be reversed
+            for (; j < k; j++) {
+                if (tail == null) break;
                 tail = tail.next;
             }
-
-            //todo
-
-
-            //break if there is no more k elements remains
+            //At last ,tail points at the last node, null or actual node
 
 
-            //reverse these k elements
+            if (tail == null) {
+                //return immediately if there is not enough nodes remain
+                return sentinel.next;
+            } else {
+                //Reverse the current k nodes,
+                //then link them to the finished work.
+                //At last, prev will point at the head of the reversed list
+                ListNode prev = null, cur = h,n = null;
+                while (prev != tail) {
+                    n = cur.next;
+                    cur.next = prev;
+                    prev = cur;
+                    cur = n;
+                }
 
-
+                t.next = tail;
+                tail = h;
+                tail.next = n;
+            }
         }
 
         return sentinel.next;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode node = Utils.createNode("[1,2,3,4,5]");
+        S25 s25 = new S25();
+        ListNode listNode = s25.reverseKGroup(node, 2);
+        System.out.println();
     }
 
 }
