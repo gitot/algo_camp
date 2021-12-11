c07002fb73bb5296afb70038f5427ea499913606
wgy <guiyong.wang@freemud.cn>
2021-12-11 13:04:31 +0800

    S83.java refactor

--- src/java/com/github/algo/base/solution/linkedlist/S83.java
+++ src/java/com/github/algo/base/solution/linkedlist/S83.java
@@ -2,7 +2,6 @@ package com.github.algo.base.solution.linkedlist;
 
 public class S83 {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null) return null;
         ListNode p = head;
         while (p != null && p.next != null) {
             if (p.next.val == p.val) {
