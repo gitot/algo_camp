3a1e690bef82bf973c02b62bf1912a4e60525cc8
wgy <guiyong.wang@freemud.cn>
2021-12-18 12:42:59 +0800

    S155.java SInterview03_05.java

--- src/java/com/github/algo/base/solution/stack/S155.java
+++ src/java/com/github/algo/base/solution/stack/S155.java
@@ -4,28 +4,36 @@ import java.util.Stack;
 
 public class S155 {
     class MinStack {
-        // TODO: 12/15/2021
-
+        private Stack<Integer> data;
+        private Stack<Integer> min;
 
         public MinStack() {
-
+            this.data = new Stack<>();
+            this.min = new Stack<>();
+            this.min.push(Integer.MAX_VALUE);
         }
 
         public void push(int val) {
-
+            data.push(val);
+            Integer min = this.min.peek();
+            if (val <= min) {
+                this.min.push(val);
+            } else {
+                this.min.push(min);
+            }
         }
 
         public void pop() {
-
+            data.pop();
+            min.pop();
         }
 
         public int top() {
-
-            return 0;
+            return data.peek();
         }
 
         public int getMin() {
-            return 0;
+            return min.peek();
         }
     }
 }
--- src/java/com/github/algo/base/solution/stack/SInterview03_05.java
+++ src/java/com/github/algo/base/solution/stack/SInterview03_05.java
@@ -1,28 +1,43 @@
 package com.github.algo.base.solution.stack;
 
+import java.util.Stack;
+
 public class SInterview03_05 {
     class SortedStack {
-        //todo
+        private Stack<Integer> data;
+        private Stack<Integer> tmp;
 
         public SortedStack() {
-
+            this.data = new Stack<>();
+            this.tmp = new Stack<>();
         }
 
         public void push(int val) {
-
+            while (!data.isEmpty() && data.peek() < val) {
+                tmp.push(data.pop());
+            }
+            data.push(val);
+            while (!tmp.isEmpty()) {
+                data.push(tmp.pop());
+            }
         }
 
         public void pop() {
-
+            if (data.isEmpty()) {
+                return;
+            }
+            data.pop();
         }
 
         public int peek() {
-
-            return 0;
+            if (data.isEmpty()) {
+                return -1;
+            }
+            return data.peek();
         }
 
         public boolean isEmpty() {
-            return false;
+            return data.isEmpty();
         }
     }
 }
