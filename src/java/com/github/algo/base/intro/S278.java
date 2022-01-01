c7b2d651af4e65917d2394f924b24eba4b17560e
wgy <guiyong.wang@freemud.cn>
2022-01-01 13:03:27 +0800

    S278.java a solution from leetcode-cn.com

--- src/java/com/github/algo/base/intro/S278.java
+++ src/java/com/github/algo/base/intro/S278.java
@@ -2,34 +2,15 @@ package com.github.algo.base.intro;
 
 public class S278 extends VersionControl {
     public int firstBadVersion(int n) {
-        int i = 1, j = n;
-        while (i <= j) {
-            if (isFirstBadVersion(i)) {
-                return i;
-            }
-            if (isFirstBadVersion(j)) {
-                return j;
-            }
-
-            int mid = (i + j) / 2;
-            if (isFirstBadVersion(mid)) {
-                return mid;
-            } else if (isBadVersion(mid)) {
-                j = mid - 1;
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
             } else {
-                i = mid + 1;
+                left = mid + 1;
             }
         }
-        return Integer.MAX_VALUE;
-    }
-
-    private boolean isFirstBadVersion(int i) {
-        if (i == 1) {
-            return isBadVersion(i);
-        }
-        if (!isBadVersion(i - 1) && isBadVersion(i)) {
-            return true;
-        }
-        return false;
+        return left;
     }
 }
