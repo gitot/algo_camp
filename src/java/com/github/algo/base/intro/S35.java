9be5287a74f997173138a22bbe69a1272d9105cc
wgy <guiyong.wang@freemud.cn>
2022-01-01 13:53:28 +0800

    S35 a solution from leetcode-cn.com

---
+++ src/java/com/github/algo/base/intro/S35.java
@@ -0,0 +1,17 @@
+package com.github.algo.base.intro;
+
+public class S35 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = ((right - left) >> 1) + left;
+            if (target <= nums[mid]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
