9e467c7e5350a50a2b8ed14dcf728f1b953ba953
guyot <1571337712@qq.com>
2022-02-11 10:19:56 +0800

    SOffer21.java

--- src/java/com/github/algo/base/solution/sort/SOffer21.java
+++ src/java/com/github/algo/base/solution/sort/SOffer21.java
@@ -2,21 +2,24 @@ package com.github.algo.base.solution.sort;
 
 public class SOffer21 {
     public int[] exchange(int[] nums) {
-        int length = nums.length;
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length - i - 1; j++) {
-                if (needBubble(nums, j)) {
-                    int tmp = nums[j+1];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = tmp;
-                }
+        int len = nums.length;
+        int left = 0,right = len - 1;
+        while (left < right) {
+            if (nums[left] % 2 != 0) {
+                left++;
+                continue;
             }
+            if (nums[right] % 2 == 0) {
+                right--;
+                continue;
+            }
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+            right--;
         }
-
         return nums;
     }
 
-    private boolean needBubble(int[] nums, int j) {
-        return nums[j] % 2 == 0 && nums[j + 1] % 2 == 1;
-    }
 }
