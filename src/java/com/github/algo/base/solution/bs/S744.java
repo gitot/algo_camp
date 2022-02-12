ea5325b8479374044f97d43f808255fffe515e9a
guyot <1571337712@qq.com>
2022-02-12 19:15:27 +0800

    S744.java A template solution for binary search problems

--- src/java/com/github/algo/base/solution/bs/S744.java
+++ src/java/com/github/algo/base/solution/bs/S744.java
@@ -2,15 +2,20 @@ package com.github.algo.base.solution.bs;
 
 public class S744 {
     public char nextGreatestLetter(char[] letters, char target) {
-        int left = 0, right = letters.length;
-        while (left < right) {
+        int left = 0, right = letters.length - 1;
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (letters[mid] <= target) {
-                left = mid + 1;
+            if (mid == 0 && letters[mid] > target) {
+                return letters[mid];
+            } else if (letters[mid] > target && letters[mid - 1] <= target) {
+                return letters[mid];
+            } else if (letters[mid] > target) {
+                right = mid - 1;
             } else {
-                right = mid;
+                left = mid + 1;
             }
         }
-        return letters[left % letters.length];
+        return letters[0];
     }
+
 }
