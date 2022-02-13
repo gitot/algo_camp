1f64a0acc3c24fcfc5da42b5ead2d66b69d8fca6
wgy <guiyong.wang@freemud.cn>
2022-02-13 14:15:34 +0800

    S367.java

---
+++ src/java/com/github/algo/base/solution/bs/S367.java
@@ -0,0 +1,19 @@
+package com.github.algo.base.solution.bs;
+
+public class S367 {
+    public boolean isPerfectSquare(int num) {
+        int left = 1, right = num;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long square = ((long) mid) * mid;
+            if (square == num) {
+                return true;
+            } else if (square > num) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+}
