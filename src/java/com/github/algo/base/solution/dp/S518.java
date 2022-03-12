dcb2987352ef92e114740bae7776c4a646c357c4
guyot <1571337712@qq.com>
2022-03-12 23:12:40 +0800

    S518.java fix problem

--- src/java/com/github/algo/base/solution/dp/S518.java
+++ src/java/com/github/algo/base/solution/dp/S518.java
@@ -5,13 +5,7 @@ public class S518 {
         int n = coins.length;
         int[][] dp = new int[n][amount + 1];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= amount; j++) {
-                dp[i][j] = Integer.MIN_VALUE;
-            }
-        }
-
-        for (int c = 0; c < amount / coins[0]; c++) {
+        for (int c = 0; c <= amount / coins[0]; c++) {
             dp[0][c * coins[0]] = c;
         }
 
@@ -19,17 +13,11 @@ public class S518 {
             for (int j = 0; j <= amount; j++) {
                 int k = amount / coins[i];
                 for (int c = 0; c <= k; c++) {
-                    if (j - c * coins[i] >= 0
-                            && dp[i - 1][j - c * coins[i]] != Integer.MIN_VALUE
-                            && dp[i - 1][j - c * coins[i]] > dp[i][j]
-                    ) {
-                        dp[i][j] = dp[i - 1][j - c * coins[i]];
-                    }
+                    dp[i][j] += dp[i - 1][j - c * coins[i]];
                 }
             }
         }
 
-        if (dp[n-1][amount] == Integer.MIN_VALUE) return 0;
         return dp[n - 1][amount];
     }
 }
