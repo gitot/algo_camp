a082887e4f474408159317bdfddb76271237efd1
guyot <1571337712@qq.com>
2022-03-13 10:07:04 +0800

    Offer47.java

---
+++ src/java/com/github/algo/base/solution/dp/Offer47.java
@@ -0,0 +1,30 @@
+package com.github.algo.base.solution.dp;
+
+public class Offer47 {
+    public int maxValue(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        dp[0][0] = grid[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                if (i - 1 < 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                    continue;
+                }
+                if (j - 1 < 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                    continue;
+                }
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
